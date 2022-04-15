584acbbe7ab15c6e77925bf60096c3c02687026d
IsabelleFSNunes <bellefsnunes@gmail.com>
2022-04-15 22:00:57 +0000

    Insert cuboid class

---
+++ ProjectsCGI/Project.03.RectangularPrism/Cuboide.cpp
@@ -0,0 +1,29 @@
+#include "Cuboide.h"
+using namespace std;
+
+ 
+   void Cuboide::Dimensions(Vertex coordinates[N_VERTICES], Vertex origin)
+    {
+       int signalX = 1;   // negative or positive signal 
+       int signalY = 1;   // negative or positive signal 
+       int signalZ = 1;   // negative or positive signal 
+
+       for (uint i = 0; i < 4; i++)
+       {
+
+           if (i / 2 != 0)
+               signalZ = -1;
+
+           coordinates[2 * i].x = origin.x + lenght / 2.0 * signalX;
+           coordinates[2 * i].y = origin.y + heigth / 2.0 * signalY;
+           coordinates[2 * i].z = origin.z + depth / 2.0 * signalZ;
+
+           signalY *= -1;
+           coordinates[2 * i + 1].x = origin.x + lenght / 2.0 * signalX;
+           coordinates[2 * i + 1].y = origin.y + heigth / 2.0 * signalY;
+           coordinates[2 * i + 1].z = origin.z + depth / 2.0 * signalZ;
+
+           signalX *= -1;
+           signalY *= -1;
+       }
+    }
---
+++ ProjectsCGI/Project.04.Table/Cuboide.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <ogldev_math_3d.h>
+
+#define N_VERTICES 8
+
+struct Vertex {
+    float x;
+    float y;
+    float z;
+    //Vector3f color;
+};
+
+
+/*
+*      p6------------- p4
+*    /  |             / |
+*   p2 ------------ p0  |
+*   |   |           |   |
+*   |   |           |   |
+*   |   p7--------- | - |
+*   |  /            | /  p5
+*   p3 ------------ p1
+*/
+// Function to generate cuboide coordinates 
+
+
+class Cuboide{       // The class
+public:                        // Access specifier
+    float lenght;
+    float heigth;
+    float depth;
+    float color[N_VERTICES];
+
+    Cuboide(float lenght, float heigth, float depth) {
+        this->lenght = lenght;
+        this->depth = depth;
+        this->heigth = heigth;
+    };
+
+    void Dimensions(Vertex coordinates[N_VERTICES], Vertex origin);
+};
