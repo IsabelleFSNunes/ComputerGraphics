1e473ef55424ac245962983367a1f7a6e96faa49
IsabelleFSNunes <bellefsnunes@gmail.com>
2022-05-16 07:40:59 +0000

    Project 01 - update, build and run correctly

--- ProjectsCGI/Project.01.Triangle/Project.01.Triangle.cpp
+++ ProjectsCGI/Project.01.Triangle/Project.01.Triangle.cpp
@@ -1,22 +1,3 @@
-/*
-
-        Copyright 2010 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Tutorial 05 - uniform variables
-*/
 
 #include <stdio.h>
 #include <string.h>
@@ -50,7 +31,7 @@ int main(int argc, char** argv)
     int x = 200;
     int y = 100;
     glutInitWindowPosition(x, y);
-    int win = glutCreateWindow("Project 02 - Cubo");
+    int win = glutCreateWindow("Project 01 - Triangle with interativity");
     printf("window id: %d\n", win);
 
     // Must be done after glut is initialized!
@@ -203,4 +184,3 @@ static void CompileShaders()
 
     glUseProgram(ShaderProgram);
 }
-
