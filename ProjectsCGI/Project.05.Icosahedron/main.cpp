36d75abdd51318df888fb9dfc5431cd80d3f3b36
IsabelleFSNunes <bellefsnunes@gmail.com>
2022-05-16 07:39:52 +0000

    Icosahedron with faces

--- ProjectsCGI/Project.05.Icosahedron/main.cpp
+++ ProjectsCGI/Project.05.Icosahedron/main.cpp
@@ -85,9 +85,9 @@ int main(int argc, char** argv)
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-    glEnable(GL_CULL_FACE);
-    glFrontFace(GL_CW);
-    glCullFace(GL_BACK);
+    // glEnable(GL_CULL_FACE);
+    // // glFrontFace(GL_CCW);
+    // glCullFace(GL_BACK);
 
     CreateVertexBuffer();
     CreateIndexBuffer();
@@ -109,59 +109,65 @@ static void RenderSceneCB()
     glClear(GL_COLOR_BUFFER_BIT);
 
     
-    static float Scale = 0.00f;
-    static float ScaleY = 0.5f;
-    static float ScaleZ = 0.5f;
+    static float angle = 0.00f;
+    static float angleY = 0.5f;
+    static float angleZ = 0.5f;
 
 
 #ifdef _WIN64
-    ScaleY += 0.0005f;
+    angle += 0.0005f;
+    angleY += 0.0005f;
+    angleZ += 0.0005f;
+
 #else
-    ScaleY += 0.02f;
+    angleY += 0.02f;
 #endif
     Matrix4f RotationX(1.0f, 0.0f, 0.0f, 0.0f,
-                        0.0f, 1.0f, cosf(Scale), sinf(Scale),
-                        0.0f, 0.0f, -sinf(Scale), cosf(Scale),
+                        0.0f, 1.0f, cosf(angle), sinf(angle),
+                        0.0f, 0.0f, -sinf(angle), cosf(angle),
                         0.0f, 0.0f, 0.0f, 1.0f);
 
-    Matrix4f RotationY ( cosf(ScaleY), 0.0f, -sinf(ScaleY), 0.0f,
+    Matrix4f RotationY ( cosf(angleY), 0.0f, -sinf(angleY), 0.0f,
                          0.0f, 1.0f, 0.0f, 0.0f,
-                        sinf(ScaleY), 0.0f, cosf(ScaleY), 0.0f,
+                        sinf(angleY), 0.0f, cosf(angleY), 0.0f,
                         0.0f, 0.0f, 0.0f, 1.0f);
 
-    Matrix4f RotationZ( cosf(ScaleZ), -sinf(ScaleZ), 0.0f, 0.0f,
-                        sinf(ScaleZ), cosf(ScaleZ), 0.0f,0.0f,
+    Matrix4f RotationZ( cosf(angleZ), -sinf(angleZ), 0.0f, 0.0f,
+                        sinf(angleZ), cosf(angleZ), 0.0f,0.0f,
                         0.0f,0.0f,1.0f,0.0f,
                         0.0f,0.0f,0.0f,1.0f);
 
-    Matrix4f Translation(1.0f, 0.0f, 0.0f, 0.0f,
-                         0.0f, 1.0f, 0.0f, 0.0f,
-                         0.0f, 0.0f, 1.0f, 6.0f,
+    float scalar = 0.25f;
+    Matrix4f Translation(scalar, 0.0f, 0.0f, 0.0f,
+                         0.0f, scalar, 0.0f, 0.0f,
+                         0.0f, 0.0f, scalar, 0.0f,
                          0.0f, 0.0f, 0.0f, 1.0f);
 
-    float VFOV = 45.0f;
-    float tanHalfVFOV = tanf(ToRadian(VFOV / 2.0f));
-    float d = 1/tanHalfVFOV;
+    // float VFOV = 45.0f;
+    // float tanHalfVFOV = tanf(ToRadian(VFOV / 2.0f));
+    // float d = 1/tanHalfVFOV;
 
-    float ar = (float)1080 / (float)840;
+    // float ar = (float)1080 / (float)840;
 
-    printf("Aspect ratio %f\n", ar);
+    // printf("Aspect ratio %f\n", ar);
 
-    float NearZ = 1.0f;
-    float FarZ = 100.0f;
+    // float NearZ = 1.0f;
+    // float FarZ = 100.0f;
 
-    float zRange = NearZ - FarZ;
+    // float zRange = NearZ - FarZ;
 
-    float A = (-FarZ - NearZ) / zRange;
-    float B = 6.0f * FarZ * NearZ / zRange;
+    // float A = (-FarZ - NearZ) / zRange;
+    // float B = 6.0f * FarZ * NearZ / zRange;
 
-    Matrix4f Projection(d/ar, 0.0f, 0.0f, 0.0f,
-                        0.0f, d,    0.0f, 0.0f,
-                        0.0f, 0.0f, A,    B,
-                        0.0f, 0.0f, 1.0f, 0.0f);
+    // Matrix4f Projection(d/ar, 0.0f, 0.0f, 0.0f,
+    //                     0.0f, d,    0.0f, 0.0f,
+    //                     0.0f, 0.0f, A,    B,
+    //                     0.0f, 0.0f, 1.0f, 0.0f);
     
 
-    Matrix4f FinalMatrix = Projection * Translation * RotationX * RotationY;
+    // Matrix4f FinalMatrix = Projection * Translation * RotationX * RotationY;
+    // Matrix4f FinalMatrix = Translation * RotationX * RotationY;
+    Matrix4f FinalMatrix = Translation * RotationX;
 
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &FinalMatrix.m[0][0]);
 
@@ -177,10 +183,11 @@ static void RenderSceneCB()
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
+    //glDrawElements(GL_LINES, 120, GL_UNSIGNED_INT, 0);
+     glDrawElements(GL_TRIANGLES, 400, GL_UNSIGNED_INT, 0);
 
-    glDrawElements(GL_LINES, 120, GL_UNSIGNED_INT, 0);
-
-    //glDrawElements(GL_TRIANGLES, 60, GL_UNSIGNED_INT, 0);
+     // glDrawElements(GL_TRIANGLES, 60, GL_UNSIGNED_INT, 0);
+     // glDrawElements(GL_TRIANGLES, 60, GL_UNSIGNED_INT, 0);
     // glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glDisableVertexAttribArray(0);
@@ -190,6 +197,7 @@ static void RenderSceneCB()
     
     glutSwapBuffers();
 }
+
 
 
 static void CreateVertexBuffer()
@@ -205,20 +213,18 @@ static void CreateVertexBuffer()
 
     // Vector with connection with GPU
     Vector3f icosahedroVector[(NELEMENTS*NVERTICES)];        // icosahedron
-   
-    // Local variables to use in adjusts before send the datas to GPU
-    Icosahedron icosahedron(1.0f); // radius
-    std::vector<float> icosahedroLocal(12*3);
-    std::vector<Vertex> icosahedroLocalSort(12);
     
+    float X = 0.525731112119133606;
+    float Z = .850650808352039932;
 
-    icosahedroLocal = icosahedron.computeVertices();
-    convertVectorVertices(&icosahedroLocal, &icosahedroLocalSort);    
+    static Vertex vdata[NVERTICES] = {
+       {-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+       {0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+       {Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+    };
 
-   // copy(icosahedron.indices.begin(), icosahedron.indices.end(), Indices.begin()); 
-    
-    for(int i = 0; i < NVERTICES ; i++){       
-        icosahedroVector[i] = Vector3f(icosahedroLocalSort.at(i).x, icosahedroLocalSort.at(i).y, icosahedroLocalSort.at(i).z );
+    for(int i = 0; i < NVERTICES ; i++){    
+         icosahedroVector[i] = Vector3f(vdata[i].x, vdata[i].y, vdata[i].z );
     }
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(icosahedroVector), icosahedroVector, GL_STATIC_DRAW);
@@ -226,59 +232,17 @@ static void CreateVertexBuffer()
 
 static void CreateIndexBuffer()
 {
- unsigned int Indices[] = {
-                                // Linhas
-                                0, 1,  1, 2,
-                                0, 2, 2, 3,
-                                0, 3, 3, 4, 
-                                0, 4, 4, 5,
-                                0, 5, 5, 1, 
-
-                                11, 10, 10, 9, 
-                                11, 9, 9, 8,
-                                11, 8, 8, 7, 
-                                11, 7, 7, 6,
-                                11, 6, 6, 10,
-
-                                1, 5, 1, 6,  
-                                2, 6, 2, 7,
-                                3, 7, 3, 8,
-                                4, 8, 4, 9,
-                                5, 9, 5, 10,
-                                1, 10,
-
-                                // Triangles - not working very well
-                               /* 0,1,2,
-                                0,2,3,
-                                0,3,4,
-                                0,4,5,
-                                0,5,1,
-
-                                11,10,9,
-                                11,9,8,
-                                11,8,7,
-                                11,7,6,
-                                11,6,10,
-
-                                6,1,5,
-                                7,2,6,
-                                8,3,7,
-                                9,8,4,
-                                10, 5, 9,
-                                10, 1, 5,*/
+    //Icosahedron icosahedron2(0.5f); // radius
+    unsigned int Indices[60]{
+               0,4,1, 0,9,4, 9,5,4, 4,5,8, 4,8,1,
+               8,10,1, 8,3,10, 5,3,8, 5,2,3, 2,7,3,
+               7,10,3, 7,6,10, 7,11,6, 11,0,6, 0,1,6,
+               6,1,10, 9,0,11, 9,11,2, 9,2,5, 7,2,11 };
 
 
-                                                                 
- };
-
-  /*for (int i = 0; i < 60; i++)
-  {
-      Indices[i] = i;
-  }*/
-
-  glGenBuffers(1, &IBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
 
 
